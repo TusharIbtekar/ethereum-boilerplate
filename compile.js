e3cb96ca95bfddc4abad5c7c3d23f11c8fabb7d2
TusharIbtekar <ibtekar8@gmail.com>
2022-08-28 21:46:27 +0600

    added compiler to compile solidity code into bytecode and ABI.

---
+++ compile.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const fs = require('fs');
+const solc = require('solc');
+
+const inboxPath = path.resolve(__dirname, 'contracts', 'Inbox.sol');
+const source = fs.readFileSync(inboxPath, 'utf-8');
+
+// console.log(solc.compile(source, 1));
+
+const input = {
+  language: 'Solidity',
+  sources: {
+    'Inbox.sol': {
+      content: source
+    }
+  },
+  settings: {
+    outputSelection: {
+      '*': {
+        '*': ['*']
+      }
+    }
+  }
+};
+
+const output = JSON.parse(solc.compile(JSON.stringify(input)))
+
+// exports.abi = output.contracts['Inbox.sol']['Inbox'].abi;
+// exports.bytecode = output.contracts['Inbox.sol']['Inbox'].evm.bytecode.object;
+
+console.log(output.contracts['Inbox.sol']['Inbox']);
