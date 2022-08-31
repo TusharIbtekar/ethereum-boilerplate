26ff1725ccf417879296bdda2e8b747501f97a46
TusharIbtekar <ibtekar8@gmail.com>
2022-09-01 02:21:22 +0600

    contract deploy testing

--- compile.js
+++ compile.js
@@ -25,7 +25,10 @@ const input = {
 
 const output = JSON.parse(solc.compile(JSON.stringify(input)))
 
-// exports.abi = output.contracts['Inbox.sol']['Inbox'].abi;
-// exports.bytecode = output.contracts['Inbox.sol']['Inbox'].evm.bytecode.object;
+// module.exports = output.contracts['Inbox.sol']['Inbox'];
+// console.log(module.exports);
 
-console.log(output.contracts['Inbox.sol']['Inbox']);
+exports.interface = output.contracts['Inbox.sol']['Inbox'].abi;
+exports.bytecode = output.contracts['Inbox.sol']['Inbox'].evm.bytecode.object;
+
+// console.log(output.contracts['Inbox.sol']['Inbox']);
