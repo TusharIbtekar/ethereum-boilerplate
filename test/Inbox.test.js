993409fd7d41cc88653f86583a5e588bcc6c1953
TusharIbtekar <ibtekar8@gmail.com>
2022-09-01 13:04:08 +0600

    testing the contract's methods before deploying to a local network

--- test/Inbox.test.js
+++ test/Inbox.test.js
@@ -6,6 +6,7 @@ const { interface, bytecode } = require('../compile');
 
 let accounts;
 let inbox;
+// const INITIAL_STRING = 'Hi there';
 
 beforeEach(async () => {
   //get a list of all accounts
@@ -19,6 +20,17 @@ beforeEach(async () => {
 
 describe('Inbox', () => {
   it('deploys a contract', () => {
-    console.log(inbox);
+    assert.ok(inbox.options.address);
   });
+  
+  it('has a default messsage', async () => {
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Hi there')
+  })
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage('Bye').send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, 'Bye');
+  })
 });
